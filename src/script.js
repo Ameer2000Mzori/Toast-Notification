eb9e5f387c442b671f15ec55cf8a97e57c3cdc59
Ameer <ameerapex496@gmail.com>
2023-12-05 06:45:14 +0100

    selected elements and created comments in typescript

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
-console.log("hallo world");
+// selecting elements
+var notigicationWrap = document.getElementsByClassName("notigication-Wrap")[0];
+var pEl = document.querySelector("p");
+var notificationButton = document.getElementsByClassName("get-Nofti-Btn")[0];
+// our objectData
+// our function
+// our event lisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("hallo world");
+// selecting elements
+const notigicationWrap = document.getElementsByClassName(
+  "notigication-Wrap"
+)[0] as any;
+const pEl = document.querySelector("p") as any;
+const notificationButton = document.getElementsByClassName(
+  "get-Nofti-Btn"
+)[0] as any;
+
+// our objectData
+
+// our function
+
+// our event lisnters
