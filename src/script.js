b483d1d865d1187c10b04b430800573a21d52c68
Ameer <ameerapex496@gmail.com>
2023-12-05 06:52:17 +0100

    added functions to the project

--- src/script.js
+++ src/script.js
@@ -1,7 +1,18 @@
 // selecting elements
 var notigicationWrap = document.getElementsByClassName("notigication-Wrap")[0];
-var pEl = document.querySelector("p");
-var notificationButton = document.getElementsByClassName("get-Nofti-Btn")[0];
+var notyBtn = document.getElementsByClassName("get-Nofti-Btn")[0];
 // our objectData
 // our function
+var showNoty = function () {
+    var pEl = document.createElement("p");
+    pEl.textContent = "hallo world";
+    pEl.style.color = "black";
+    notigicationWrap.classList.add("active");
+    notigicationWrap.style.backgroundColor = "green";
+    notigicationWrap.appendChild(pEl);
+    setTimeout(function () {
+        notigicationWrap.classList.remove("active");
+    }, 1500);
+};
 // our event lisnters
+notyBtn.addEventListener("click", showNoty);
--- src/script.ts
+++ src/script.ts
@@ -2,13 +2,26 @@
 const notigicationWrap = document.getElementsByClassName(
   "notigication-Wrap"
 )[0] as any;
-const pEl = document.querySelector("p") as any;
-const notificationButton = document.getElementsByClassName(
-  "get-Nofti-Btn"
-)[0] as any;
+
+const notyBtn = document.getElementsByClassName("get-Nofti-Btn")[0];
 
 // our objectData
 
 // our function
+const showNoty = () => {
+  const pEl = document.createElement("p");
+
+  pEl.textContent = "hallo world";
+  pEl.style.color = "black";
+
+  notigicationWrap.classList.add("active");
+  notigicationWrap.style.backgroundColor = "green";
+  notigicationWrap.appendChild(pEl);
+
+  setTimeout(() => {
+    notigicationWrap.classList.remove("active");
+  }, 1500);
+};
 
 // our event lisnters
+notyBtn.addEventListener("click", showNoty);
